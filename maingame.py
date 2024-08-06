d3fab59a252bd30a4c2e11a03a184a3ff4361a9b
Arsam Amini <arsam.the.amini@gmail.com>
2024-08-06 16:23:19 +0330

    maingame basics are ready

--- maingame.py
+++ maingame.py
@@ -10,4 +10,23 @@ pygame.display.set_caption("Game")
 # classes
 class player(object):
     def __init__(self, maxl, maxc, width, hight, color):
+        self.maxl = maxl
+        self.maxc = maxc
+        self.width = width
+        self.hight = hight
+        self.color = color
+    
+    def draw():
         pass
+
+    def move():
+        pass
+
+# redraw
+def redeawwindow():
+    pass
+
+# main loop
+run = True
+while run:
+    pass
