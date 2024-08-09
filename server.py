0f059f7a8c6b8b6b158c1020104849207120d620
Arsam Amini <arsam.the.amini@gmail.com>
2024-08-09 17:50:03 +0330

    just wrote rate limiting for server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
 import sys
+import time
 
 server = "localhost"
 port = 8888
@@ -15,28 +16,39 @@ except socket.error as e:
 listening_socket.listen(4)
 print("Waiting for a connection")
 
+TIME_WINDOW = 10
+MAX_REQUESTS = 5
+request_times = {}
+
 def threaded_client(conn):
     conn.send(str.encode("Connected, this is my message"))
     reply = ""
     while True:
         try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
-
+            data = conn.recv(2048) # receive data
             if not data:
-                print("Disconnected")
+                print("Disconnected") # disconnect if there is no data
                 break
             else:
-                print("Received: ", reply)
-                print("Sending: ", reply)
-
-                conn.sendall(str.encode(reply))
+                current_time = time.time() # get current time for ddos controlling
+                if addr[0] not in request_times: # if this is a new ip
+                    request_times[addr[0]] = [] # add this ip into request_times and give it a list
+                request_times[addr[0]] = [t for t in request_times[addr[0]] if current_time - t < TIME_WINDOW] # if the last request of this ip was more than 10 seconds ago filter it
+                if len(request_times[addr[0]]) < MAX_REQUESTS: # if the len of this ip requests were less than 5
+                    request_times[addr[0]].append(current_time) # add now to the list
+                    reply = data.decode("utf-8") # everything is ok so decode data
+                    print("Received: ", reply)
+                    print("Sending: ", reply)
+                    conn.sendall(str.encode(reply)) # then encode again and send
+                    return False
+                else:
+                    return True
         except Exception as e:
             print(f"this the error : {e}")
             break
 
-        print("Connection lost")
-        conn.close()
+    print("Connection lost")
+    conn.close()
 
 while True:
     conn, addr = listening_socket.accept()
